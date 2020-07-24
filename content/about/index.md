84e1ad05266765028fe444336b4cc31f76bdc5f2
Rajiv Kumar <rajivk@gentrack.com>
2020-07-24 23:57:37 +0100

    adding about me

---
+++ content/about/index.md
@@ -0,0 +1,21 @@
+---
+title: 'About Me'
+avatar: './me.jpg'
+skills:
+  - Python
+  - Java 8
+  - C#
+  - SQL
+  - React
+  - Node.js
+  - HTML & (S)CSS
+
+---
+
+Hello! I'm Rajiv, a software engineer based in London.
+
+I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to produce innovative and sustainable solutions for my clients all while providing a great user experience.
+
+Shortly after graduating from [University of Auckland](https://www.auckland.ac.nz/en.html), I joined the operations team at [Gentrack](https://www.gentrack.com/) where I work on a wide variety of interesting projects in the Utilities industry.
+
+Here are a few technologies I've been working with recently:
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: `Rajiv Kumar | Software Engineer`,
     description: `Child theme and starter from gatsby-theme-catalyst-core`,
-    author: `Eric Howey`,
+    author: `Rajiv Kumar`,
     siteUrl: `http://localhost:8000/`, //Change to you site address, required for sitemap.xml and robots.txt file
     menuLinks: [
       {
@@ -52,8 +52,8 @@ module.exports = {
         name: `gatsby-starter-catalyst-onepage`,
         short_name: `catalyst-onepage`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#000000`,
+        background_color: `#0a192f`,
+        theme_color: `#0a192f`,
         display: `minimal-ui`,
         icon: `src/images/logo-512.png` // This path is relative to the root of the site.
       }
